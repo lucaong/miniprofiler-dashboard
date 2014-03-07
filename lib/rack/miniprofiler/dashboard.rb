9ce80d0c1e59c7d0250c4404fd430b5d3cc17a0a
Luca Ongaro <lukeongaro@gmail.com>
2014-03-07 16:03:40 +0100

    absolute path for public folder

--- lib/rack/miniprofiler/dashboard.rb
+++ lib/rack/miniprofiler/dashboard.rb
@@ -2,7 +2,8 @@ module Rack
   class MiniProfiler
     class Dashboard < Rack::Static
       def initialize( app )
-        super( app, urls: [""], root: "public", index: "index.html" )
+        public_path = ::File.expand_path( '../../../public', ::File.dirname( __FILE__ ) )
+        super( app, urls: [''], root: public_path, index: 'index.html' )
       end
     end
   end
