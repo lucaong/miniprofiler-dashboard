34583bb0d5640a36f4025d6367abee9e0c15941c
Luca Ongaro <lukeongaro@gmail.com>
2014-03-07 15:40:23 +0100

    fix initialize signature

--- lib/rack/miniprofiler/dashboard.rb
+++ lib/rack/miniprofiler/dashboard.rb
@@ -1,8 +1,8 @@
 module Rack
   class MiniProfiler
     class Dashboard < Rack::Static
-      def initialize
-        super( urls: [""], root: "public", index: "index.html" )
+      def initialize( app )
+        super( app, urls: [""], root: "public", index: "index.html" )
       end
     end
   end
